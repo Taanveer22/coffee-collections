0be4d56bbbdfb06148e437b6bd561420fcf470cd
Taanveer22 <taanveer469@gmail.com>
2025-11-04 04:02:52 +0600

    alhamdulliah coffees showed by category in the home

--- src/components/AllCards.jsx
+++ src/components/AllCards.jsx
@@ -1,16 +1,24 @@
 import { useLoaderData, useParams } from "react-router-dom";
 import Card from "./Card";
+import { useEffect, useState } from "react";
 
 const AllCards = () => {
   const { category } = useParams();
-  console.log(category);
+  // console.log(category);
   const coffeesData = useLoaderData();
   // console.log(coffeesData);
+  const [coffees, setCoffees] = useState([]);
+  useEffect(() => {
+    const filteredByCategory = [...coffeesData].filter(
+      (coffeeItem) => coffeeItem.category === category
+    );
+    setCoffees(filteredByCategory);
+  }, [coffeesData, category]);
+  // console.log(coffees);
   return (
     <div>
-      <h1>cards ... {category}</h1>
-      <div className="grid gap-6 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3">
-        {coffeesData.map((coffeeItem) => (
+      <div className="grid gap-6 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 mt-6">
+        {coffees.map((coffeeItem) => (
           <Card key={coffeeItem.id} coffeeItem={coffeeItem}></Card>
         ))}
       </div>
